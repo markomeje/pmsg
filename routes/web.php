9f76b76b49501aff377363362b4ebc5924a68d6b
markomeje <markomejeonline@gmail.com>
2022-06-09 12:59:12 +0100

    Remove Admin url

--- routes/web.php
+++ routes/web.php
@@ -14,32 +14,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::domain(env('APP_URL'))->group(function() {
-    Route::middleware(['web'])->group(function() {
-        Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-        Route::get('/support', [\App\Http\Controllers\HomeController::class, 'support'])->name('support');
-        Route::get('/profile', [\App\Http\Controllers\HomeController::class, 'profile'])->name('profile');
+Route::middleware(['web'])->domain(env('APP_URL'))->group(function() {
+    Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/support', [\App\Http\Controllers\HomeController::class, 'support'])->name('support');
+    Route::get('/profile', [\App\Http\Controllers\HomeController::class, 'profile'])->name('profile');
 
-        Route::prefix('supporter')->group(function () {
-            Route::post('/add', [\App\Http\Controllers\SupporterController::class, 'add'])->name('supporter.add');
-        });
-
-        Route::prefix('news')->group(function () {
-            Route::get('/', [\App\Http\Controllers\NewsController::class, 'index'])->name('news');
-            Route::post('/{id}/{slug}', [\App\Http\Controllers\NewsController::class, 'send'])->name('news.read');
-        });
-
-        Route::group(['middleware' => 'guest'], function () {
-            Route::get('/login', [\App\Http\Controllers\LoginController::class, 'login'])->name('login');
-            Route::post('/auth', [\App\Http\Controllers\LoginController::class, 'auth'])->name('auth.login');
-            Route::get('/logout', [\App\Http\Controllers\LoginController::class, 'logout'])->name('logout');
-        });
-    });    
-});
-
-Route::domain(env('ADMIN_URL'))->group(function() {
-    Route::middleware(['web', 'auth'])->group(function() {
-        Route::get('/', [\App\Http\Controllers\AdminController::class, 'index'])->name('admin');
+    Route::prefix('supporter')->group(function () {
+        Route::post('/add', [\App\Http\Controllers\SupporterController::class, 'add'])->name('supporter.add');
     });
-});
+
+    Route::prefix('news')->group(function () {
+        Route::get('/', [\App\Http\Controllers\NewsController::class, 'index'])->name('news');
+        Route::post('/{id}/{slug}', [\App\Http\Controllers\NewsController::class, 'send'])->name('news.read');
+    });
+
+    Route::group(['middleware' => 'guest'], function () {
+        Route::get('/login', [\App\Http\Controllers\LoginController::class, 'login'])->name('login');
+        Route::post('/auth', [\App\Http\Controllers\LoginController::class, 'auth'])->name('auth.login');
+        Route::get('/logout', [\App\Http\Controllers\LoginController::class, 'logout'])->name('logout');
+    });
+});    
+
+// Route::domain(env('ADMIN_URL'))->group(function() {
+//     Route::middleware(['web', 'auth'])->group(function() {
+//         Route::get('/', [\App\Http\Controllers\AdminController::class, 'index'])->name('admin');
+//     });
+// });
 
