1f606870f229225b54dcbe4213ffc395f72a112e
markomeje <markomejeonline@gmail.com>
2022-06-09 08:18:56 +0100

    Fix Helpers Issue

--- app/helpers.php
+++ app/helpers.php
@@ -1,3 +1,6 @@
+<?php
+
+
 if (!function_exists('randomrgba')) {
     function randomrgba($opacity = 0.5) {
         return 'rgba('.rand(0, 255).','. rand(0, 255).','. rand(0, 255).','. $opacity.')';
